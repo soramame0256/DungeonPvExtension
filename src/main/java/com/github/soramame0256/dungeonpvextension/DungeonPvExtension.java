2eac294fd048efce09931b066b31d4f971b3d06e
soramame0256 <sora200507@gmail.com>
2022-02-24 23:39:42 +0900

    1.0.4-beta.2 update  cost list added

--- src/main/java/com/github/soramame0256/dungeonpvextension/DungeonPvExtension.java
+++ src/main/java/com/github/soramame0256/dungeonpvextension/DungeonPvExtension.java
@@ -24,7 +24,7 @@ public class DungeonPvExtension {
 
     public static final String MOD_ID = "dungeonpvextension";
     public static final String MOD_NAME = "DungeonPvExtension";
-    public static final String VERSION = "1.0.3";
+    public static final String VERSION = "1.0.4-beta.2";
     public static KeyBinding[] keyBindings = new KeyBinding[3];
     public static boolean inDP = false;
     public static boolean isEnable = true;
