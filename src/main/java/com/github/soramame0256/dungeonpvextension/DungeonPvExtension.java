9a79e7a2ffc0b8fc958844ad58ae19c5730a92ff
soramame0256 <sora200507@gmail.com>
2022-06-02 19:04:01 +0900

    よくわからん 修正出来なさそう

--- src/main/java/com/github/soramame0256/dungeonpvextension/DungeonPvExtension.java
+++ src/main/java/com/github/soramame0256/dungeonpvextension/DungeonPvExtension.java
@@ -37,7 +37,7 @@ public class DungeonPvExtension {
 
     public static final String MOD_ID = "dungeonpvextension";
     public static final String MOD_NAME = "DungeonPvExtension";
-    public static final String VERSION = "1.0.16b";
+    public static final String VERSION = "1.0.16c";
     private static DataUtils dataUtil;
     public static KeyBinding[] keyBindings = new KeyBinding[8];
     public static boolean inDP = false;
